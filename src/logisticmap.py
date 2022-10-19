179b0123ca3773afe23cca56dc7d21bc07d0b30a
Erik Storås Sommer <erikssommer@gmail.com>
2022-10-19 13:37:20 +0200

    Added dockstr

--- src/logisticmap.py
+++ src/logisticmap.py
@@ -5,29 +5,36 @@ import sys
 # Load plotting library
 import matplotlib.pyplot as plt
 
-# Function that creates a series of numbers using chaos theory
+
 def logistic_map(x0: float, rate: float, n: int) -> tuple:
+    """
+    Function that creates a series of numbers using chaos theory
+    """
     x_values = []
-    r = rate # Rate, (The Edge of Chaos (r ≈ 3.57))
-    x = x0 # Initial value
-    
+    r = rate  # Rate, (The Edge of Chaos (r ≈ 3.57))
+    x = x0  # Initial value
+
     # Generate all values of x given the initial value
     for i in range(n):
-        x_values += [x] # Add the number to the list
-        x = x * r * (1 - x) # Logistic map equation
-    
+        x_values += [x]  # Add the number to the list
+        x = x * r * (1 - x)  # Logistic map equation
+
     # Return x and y
     return np.arange(len(x_values)), x_values
 
-# Function that plot a (x, y) series of points
+
 def plot_line_chart(x: int, y: float, x_label: str, y_label: str, title: str) -> None:
-    plt.figure(figsize = (10, 4))
-    plt.plot(x, y, label = y_label)
-    plt.xlabel(x_label, fontsize = 10)
-    plt.ylabel(y_label, fontsize = 10)
+    """
+    Function that plot a (x, y) series of points
+    """
+    plt.figure(figsize=(10, 4))
+    plt.plot(x, y, label=y_label)
+    plt.xlabel(x_label, fontsize=10)
+    plt.ylabel(y_label, fontsize=10)
     plt.title(title, size=14)
-    plt.legend(loc = 'upper right')
+    plt.legend(loc='upper right')
     plt.show()
+
 
 # Create and plot 100 random-chaos numbers given initial value and rate from shell input
 init_value_x = sys.argv[1]
