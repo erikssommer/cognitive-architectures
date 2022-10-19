6f790f242f7c4eee17c9376f11d90c031c4c7dcc
Erik Storås Sommer <erikssommer@gmail.com>
2022-10-19 12:49:37 +0200

    Added input from shell

--- logisticmap.py
+++ logisticmap.py
@@ -1,13 +1,14 @@
 # Load math library
 import numpy as np
-
+# Load system library
+import sys
 # Load plotting library
 import matplotlib.pyplot as plt
 
 # Function that creates a series of numbers using chaos theory
-def logistic_map(x0: float, n: int) -> list:
+def logistic_map(x0: float, rate: float, n: int) -> list:
     x_values = []
-    r = 3.9 # Rate, (The Edge of Chaos (r ≈ 3.57))
+    r = rate # Rate, (The Edge of Chaos (r ≈ 3.57))
     x = x0 # Initial value
     
     # Generate all values of x given the initial value
@@ -19,16 +20,19 @@ def logistic_map(x0: float, n: int) -> list:
     return np.arange(len(x_values)), x_values
 
 # Function that plot a (x, y) series of points
-def plot_line_chart(x, y, x_label, y_label, title):
-    plt.figure(figsize = (16, 4))
+def plot_line_chart(x: int, y: float, x_label: str, y_label: str, title: str) -> None:
+    plt.figure(figsize = (10, 4))
     plt.plot(x, y, label = y_label)
-    plt.xlabel(x_label, fontsize = 11)
-    plt.ylabel(y_label, fontsize = 11)
+    plt.xlabel(x_label, fontsize = 10)
+    plt.ylabel(y_label, fontsize = 10)
     plt.title(title, size=14)
     plt.legend(loc = 'upper right')
     plt.show()
 
-# Create and plot 100 random-chaos numbers
-x0 = 0.7
-x, y = logistic_map(x0, 100)
-plot_line_chart(x, y, '$x$', '$chaos(x)$', 'Plot created with the logistic map chaos function')
+# Create and plot 100 random-chaos numbers given initial value
+init_value_x = sys.argv[1]
+x0 = float(init_value_x)
+rate = sys.argv[2]
+r = float(rate)
+x, y = logistic_map(x0, r, 100)
+plot_line_chart(x, y, '$x$', '$chaos(x)$', 'Initial value: {}, Rate: {}'.format(x0, rate))
