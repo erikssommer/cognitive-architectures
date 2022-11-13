647a9867167d125469e402d5bbf03eb138a8e78d
Erik Storås Sommer <erikssommer@gmail.com>
2022-11-13 20:29:40 +0100

    Commenting

--- src/ann/perceptron.py
+++ src/ann/perceptron.py
@@ -13,12 +13,16 @@ class Perceptron:
         self.learning_rate = learning_rate
         self.epochs = epochs  # number of iterations
 
-    # Step function for the activation
     def activation(self, input):
+        """
+        Step function for the activation function
+        """
         return 1.0 if input > 0 else 0.0
 
-    # Perceptron learning rule
     def train(self):
+        """
+        Perceptron learning rule
+        """
 
         _, features = self.inputs.shape
 
@@ -33,9 +37,9 @@ class Perceptron:
         for _ in range(self.epochs):
 
             # counter to store misclassified.
-            n_miss = 0
+            error_counter = 0
 
-            # looping for every example
+            # Looping for every example
             for idx, x_i in enumerate(self.inputs):
 
                 # Insering 1 for bias, X0 = 1
@@ -50,15 +54,18 @@ class Perceptron:
                         ((self.learning_data[idx] - y_hat)*x_i)
 
                     # Incrementing by 1
-                    n_miss += 1
+                    error_counter += 1
 
             # Appending number of misclassified examples at every iteration.
-            misclassified.append(n_miss)
+            misclassified.append(error_counter)
 
         return theta, misclassified
 
 
 def readFile(filename):
+    """
+    Read the file and return the data as a numpy array with inputs and desired outputs.
+    """
     with open(f'../../data/{filename}.csv') as csvfile:
         readCSV = pd.read_csv(csvfile, skiprows=1,
                               header=None, delimiter=',').to_numpy()
@@ -72,52 +79,64 @@ def readFile(filename):
         return np.array(inputs), np.array(desired)
 
 
-def clean_dataset():
-    shutil.copy('../../data/dataset.csv', '../../data/dataset_clean.csv')
+def clean_dataset(filename):
+    """
+    Clean the dataset to make it linearly separable
+    """
+    # Retrieving the original filename
+    original_filename = filename.split('_', 1)[0]
+    # Making a copy of the original file
+    shutil.copy(f'../../data/{original_filename}.csv', f'../../data/{filename}.csv')
 
-    df = pd.read_csv('../../data/dataset_clean.csv')
+    df = pd.read_csv(f'../../data/{filename}.csv')
 
-    # update third column to 1 if first column is negative
+    # Update third column to 1 if first column is negative
     for index, row in df.iterrows():
         if row[0] < 0:
             df.at[index, 'y'] = 1
         else:
             df.at[index, 'y'] = 0
 
-    # save to csv
+    # Save to csv
     df.to_csv('../../data/dataset_clean.csv', index=False)
 
 
 def plot_decision_boundary(inputs, y, theta, filename):
 
     # Equation ax+b = theta0.X0 + theta1.X1 + theta2.X2
-    x1 = [min(inputs[:, 0]), max(inputs[:, 0])]
     a = -theta[1]/theta[2]
     b = -theta[0]/theta[2]
+    x1 = [min(inputs[:, 0]), max(inputs[:, 0])]
     x2 = a * x1 + b
 
     # Plotting the decision boundary
     plt.figure(figsize=(10, 8))
-    plt.plot(inputs[:, 0][y == 0], inputs[:, 1][y == 0], "o", color="red")
-    plt.plot(inputs[:, 0][y == 1], inputs[:, 1][y == 1], "o", color="blue")
     plt.title('Perceptron Decision Boundary')
     plt.xlabel('x1')
     plt.ylabel('x2')
+    plt.plot(inputs[:, 0][y == 0], inputs[:, 1][y == 0], "o", color="red")
+    plt.plot(inputs[:, 0][y == 1], inputs[:, 1][y == 1], "o", color="blue")
     plt.plot(x1, x2, color='black')
     plt.savefig(f'../../images/{filename}.png')
     plt.show()
 
 
 if __name__ == '__main__':
+    # Dataset filename to be used
     filename = sys.argv[1]
 
-    if filename == 'dataset_clean':
-        clean_dataset()
+    # Clean dataset to make it linearly separable
+    if 'clean' in filename:
+        clean_dataset(filename)
 
+    # Reading the dataset and formatting it
     inputs, desired = readFile(filename)
 
+    # Initializing the Perceptron
     perceptron = Perceptron(inputs, desired, 0.5, 100)
 
+    # Training the Perceptron
     theta, misclassified = perceptron.train()
 
+    # Plotting the decision boundary
     plot_decision_boundary(inputs, desired, theta, filename)
